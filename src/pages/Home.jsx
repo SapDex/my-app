8735a2facc6031a22336a77832443c5725d015e2
SapDex <gabrielkozakiewcz96@gmail.com>
2025-09-27 11:53:12 +0100

    27Sep_Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header.jsx';
+function Home() {
+    return (
+    <div>
+        <Header />  
+        <p>Hello, this is your home page!</p>
+        </div>
+    );
+}
+export default Home;
